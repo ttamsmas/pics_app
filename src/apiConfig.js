26f152d6c976e9c45aa5fbc17a4d8e4f2e954d60
ttamsmas <ttamsmas@gmail.com>
2020-12-18 13:10:13 -0800

    Updating Development Port to 8000

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
   production: 'https://aqueous-atoll-85096.herokuapp.com',
-  development: 'http://localhost:7165'
+  development: 'http://localhost:8000'
 }
 
 if (window.location.hostname === 'localhost') {
