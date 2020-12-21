10583265218b0767148916bdd43902c14d9bff92
ttamsmas <ttamsmas@gmail.com>
2020-12-21 10:41:44 -0800

    Reversing API Debug

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://git.heroku.com/pics-api2020.git',
+  production: 'https://aqueous-atoll-85096.herokuapp.com',
   development: 'http://localhost:8000'
 }
 
