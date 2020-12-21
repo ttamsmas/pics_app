9b8996a314fcc0d492f26d6194a1c674836292d9
ttamsmas <ttamsmas@gmail.com>
2020-12-21 10:34:46 -0800

    debugging production url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://git.heroku.com/pics-api2020.git',
   development: 'http://localhost:8000'
 }
 
