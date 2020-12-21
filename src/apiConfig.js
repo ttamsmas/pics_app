4bb311b08966602c0c9a7697f651e2900bc19175
ttamsmas <ttamsmas@gmail.com>
2020-12-21 11:05:34 -0800

    reversing last commit

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://pics-api2020.herokuapp.com',
+  production: 'https://aqueous-atoll-85096.herokuapp.com',
   development: 'http://localhost:8000'
 }
 
